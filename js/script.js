7048b8d385fd9c2ebae29fa3fb3d02ec8aa78f59
Haitch <halidbajrami12@gmail.com>
2025-12-17 21:05:56 +1100

    5
    fully finished website HTML, CSS AND JS
    
    going to upload website and test how it goes. still need to setup the contact form to send to my business email.

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ document.querySelectorAll('.email-link').forEach(link => {
     const domain = link.getAttribute('data-domain');
     link.href = 'mailto:' + user + '@' + domain;
 });
+
+
+// Hamburger menu toggle
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('show');
+});
+
+
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', () => {
+      hamburger.classList.remove('active');
+      navMenu.classList.remove('show');
+    });
+});
