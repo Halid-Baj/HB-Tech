dc63defcc47f0e4a487ed41295ba7cf32d5728a1
Haitch <halidbajrami12@gmail.com>
2025-12-18 21:17:39 +1100

    finished contact form submission
    removed the white thin border around all sections apart. i think just a box shadow does just fine.
    
    used FormSubmit to send contact form to specified email.

--- js/script.js
+++ js/script.js
@@ -23,3 +23,47 @@ document.querySelectorAll('nav a').forEach(link => {
       navMenu.classList.remove('show');
     });
 });
+
+
+// Form Validation
+
+const fname = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const form = document.getElementById('contact-form');
+const errorElement = document.getElementById('error');
+
+const errorName = fname.nextElementSibling;
+const errorEmail = email.nextElementSibling;
+const errorMessage = message.nextElementSibling;
+
+form.addEventListener('submit', (e) => {
+    errorName.textContent = '';
+    errorEmail.textContent = '';
+    errorMessage.textContent = '';
+
+    let isValid = true;
+
+    if (fname.value.trim() === '') {
+        errorName.textContent = 'Name is required';
+        isValid = false;
+    }
+
+    if (email.value.trim() === '') {
+        errorEmail.textContent = 'Email is required';
+        isValid = false;
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value.trim())) {
+        errorEmail.textContent = 'Email is invalid';
+        isValid = false;
+    }
+
+    if (message.value.trim() === '') {
+        errorMessage.textContent = 'Message is required';
+        isValid = false;
+    }
+
+    if (!isValid) {
+        e.preventDefault();
+    }
+
+});
